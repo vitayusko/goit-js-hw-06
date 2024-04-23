31b41d5efebc7ff6d3fbaedf1d40ac788f81e8c7
vitayusko <vitayusko@users.noreply.github.com>
2024-04-23 13:41:05 +0200

    23/04

---
+++ js/task-3.js
@@ -0,0 +1,62 @@
+class StringBuilder {
+  #initialValue;
+
+  constructor(initialValue) {
+    this.#initialValue = initialValue;
+  }
+
+  getValue() {
+    return this.#initialValue;
+  }
+
+  padEnd(str) {
+    this.#initialValue += str;
+  }
+
+  padStart(str) {
+    this.#initialValue = str + this.#initialValue;
+  }
+
+  padBoth(str) {
+    this.#initialValue = str + this.#initialValue + str;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+
+
+
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, 
+// який записується у приватну властивість value об'єкта, що створюється.
+
+
+
+// // Оголоси наступні методи класу:
+
+// // getValue() — повертає поточне значення приватної властивості value.
+// // padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної 
+// властивості value об'єкта, який викликає цей метод.
+// // padStart(str) — отримує параметр str (рядок) і додає його на початок значення 
+// приватної властивості value об'єкта, який викликає цей метод.
+// // padBoth(str) — отримує параметр str (рядок) і додає його на початок і в 
+// кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав 
+// його після оголошення класу для перевірки коректності роботи.У консоль
+//  будуть виведені результати їх роботи.Будь ласка, нічого там не змінюй.
